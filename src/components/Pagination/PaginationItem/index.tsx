8419b14821047def9934b9041d764638fa427ef1
renan <renan@interprocess.com.br>
2022-01-28 16:52:17 -0300

    chore(PaginationItem): corrige carregamento dos botões

--- src/components/Pagination/PaginationItem/index.tsx
+++ src/components/Pagination/PaginationItem/index.tsx
@@ -4,37 +4,29 @@ import { PaginationItemProps } from "./interface";
 
 export const PaginationItem: React.FC<PaginationItemProps> = ({ number, isCurrent = false }) => {
 
-    const renderButtonCurrent = useCallback(() => {
-        return <Button
-            size="sm"
-            fontSize="xs"
-            width="4"
-            colorScheme="pink"
-            disabled
-            _disabled={{ bgColor: 'pink.500', cursor: 'default' }}
-        >
-            {number}
-        </Button>
-    }, [number])
-
-    const renderButtonDefault = useCallback(() => {
-        return <Button
-            size="sm"
-            fontSize="xs"
-            width="4"
-            bg="gray.700"
-            _hover={{ bg: 'gray.500' }}
-        >
-            {number}
-        </Button>
-    }, [number])
-
-
     return (
         <>
             {isCurrent ?
-                renderButtonCurrent :
-                renderButtonDefault
+                <Button
+                    size="sm"
+                    fontSize="xs"
+                    width="4"
+                    colorScheme="pink"
+                    disabled
+                    _disabled={{ bgColor: 'pink.500', cursor: 'default' }}
+                >
+                    {number}
+                </Button>
+                :
+                <Button
+                    size="sm"
+                    fontSize="xs"
+                    width="4"
+                    bg="gray.700"
+                    _hover={{ bg: 'gray.500' }}
+                >
+                    {number}
+                </Button>
             }
         </>
     );
