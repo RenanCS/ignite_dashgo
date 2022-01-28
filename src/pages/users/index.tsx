fbc6df0f5c037a84fa2f600155e2cd19074deca1
renan <renan@interprocess.com.br>
2022-01-28 16:52:52 -0300

    chore(Users): adiciona lógica para carregar os dados do usuário na tela

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Button, Checkbox, Flex, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { getUsers } from "controllers/getUsers";
 import { NextPage } from "next";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
@@ -6,8 +7,14 @@ import { Header } from "src/components/Header";
 import { Heading } from "src/components/Heading";
 import { Pagination } from "src/components/Pagination";
 import { Menu } from "src/components/Sidebar/Menu";
+import { useQuery } from "react-query";
 
 const Users: NextPage = () => {
+
+    const { data, isLoading, error } = useQuery('users', async () => {
+        const users = await getUsers();
+        return users;
+    })
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -38,97 +45,109 @@ const Users: NextPage = () => {
                             </Button>
                         </Link>
                     </Flex>
+                    {
+                        isLoading ?
+                            (<Flex justify="center"><Spinner /></Flex>) :
+                            error ? (
+                                <Flex justify="center">
+                                    <Text>Falhaao obter dados do usuário</Text>
+                                </Flex>
+                            ) :
+                                (
+                                    <>
+                                        <Table colorScheme="whiteAlpha">
+                                            <Thead>
+                                                <Tr>
+                                                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                                                        <Checkbox colorScheme="pink" />
+                                                    </Th>
+                                                    <Th>
+                                                        Usuário
+                                                    </Th>
+                                                    {isWideVersion &&
+                                                        <Th>
+                                                            Data Cadastro
+                                                        </Th>
+                                                    }
 
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>
-                                    Usuário
-                                </Th>
-                                {isWideVersion &&
-                                    <Th>
-                                        Data Cadastro
-                                    </Th>
-                                }
+                                                    <Th>
+                                                    </Th>
+                                                </Tr>
+                                            </Thead>
+                                            <Tbody>
+                                                <Tr>
+                                                    <Td px={["4", "4", "6"]}>
+                                                        <Checkbox colorScheme="pink" />
+                                                    </Td>
+                                                    <Td>
+                                                        <Box>
+                                                            <Text fontWeight="bold">Renan Carvalho</Text>
+                                                            <Text fontSize="sm" color="gray.300">rcsti.dev@gmail.com</Text>
+                                                        </Box>
+                                                    </Td>
+                                                    {isWideVersion &&
+                                                        <Td>
+                                                            04 de Abril, 2021
+                                                        </Td>
+                                                    }
+                                                    <Td>
+                                                        <Button as="a" size="sm" fontSize="sm" colorScheme="purple"
+                                                            leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
+                                                            {isWideVersion && 'Editar'}
+                                                        </Button>
+                                                    </Td>
+                                                </Tr>
+                                                <Tr>
+                                                    <Td px={["4", "4", "6"]}>
+                                                        <Checkbox colorScheme="pink" />
+                                                    </Td>
+                                                    <Td>
+                                                        <Box>
+                                                            <Text fontWeight="bold">Renan Carvalho</Text>
+                                                            <Text fontSize="sm" color="gray.300">rcsti.dev@gmail.com</Text>
+                                                        </Box>
+                                                    </Td>
+                                                    {isWideVersion &&
+                                                        <Td>
+                                                            04 de Abril, 2021
+                                                        </Td>
+                                                    }
+                                                    <Td>
+                                                        <Button as="a" size="sm" fontSize="sm" colorScheme="purple"
+                                                            leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
+                                                            {isWideVersion && 'Editar'}
+                                                        </Button>
+                                                    </Td>
+                                                </Tr>
+                                                <Tr>
+                                                    <Td px={["4", "4", "6"]}>
+                                                        <Checkbox colorScheme="pink" />
+                                                    </Td>
+                                                    <Td>
+                                                        <Box>
+                                                            <Text fontWeight="bold">Renan Carvalho</Text>
+                                                            <Text fontSize="sm" color="gray.300">rcsti.dev@gmail.com</Text>
+                                                        </Box>
+                                                    </Td>
+                                                    {isWideVersion &&
+                                                        <Td>
+                                                            04 de Abril, 2021
+                                                        </Td>
+                                                    }
+                                                    <Td>
 
-                                <Th>
-                                </Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Renan Carvalho</Text>
-                                        <Text fontSize="sm" color="gray.300">rcsti.dev@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion &&
-                                    <Td>
-                                        04 de Abril, 2021
-                                    </Td>
-                                }
-                                <Td>
-                                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple"
-                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
-                                        {isWideVersion && 'Editar'}
-                                    </Button>
-                                </Td>
-                            </Tr>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Renan Carvalho</Text>
-                                        <Text fontSize="sm" color="gray.300">rcsti.dev@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion &&
-                                    <Td>
-                                        04 de Abril, 2021
-                                    </Td>
-                                }
-                                <Td>
-                                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple"
-                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
-                                        {isWideVersion && 'Editar'}
-                                    </Button>
-                                </Td>
-                            </Tr>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Renan Carvalho</Text>
-                                        <Text fontSize="sm" color="gray.300">rcsti.dev@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion &&
-                                    <Td>
-                                        04 de Abril, 2021
-                                    </Td>
-                                }
-                                <Td>
-
-                                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple"
-                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
-                                        {isWideVersion && 'Editar'}
-                                    </Button>
-                                </Td>
-                            </Tr>
-                        </Tbody>
-                    </Table>
-                    <Pagination />
+                                                        <Button as="a" size="sm" fontSize="sm" colorScheme="purple"
+                                                            leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
+                                                            {isWideVersion && 'Editar'}
+                                                        </Button>
+                                                    </Td>
+                                                </Tr>
+                                            </Tbody>
+                                        </Table>
+                                        <Pagination />
+                                    </>
+                                )
+                    }
                 </Box>
             </Flex >
         </Box >
