56a0d38a0ef120c541327f27a15d923309d9ccc1
renan <renan@interprocess.com.br>
2022-01-28 09:52:37 -0300

    chore(ActiveLink): altera  interface limpando componente não utilizado

--- src/components/ActiveLink/interface.tsx
+++ src/components/ActiveLink/interface.tsx
@@ -2,7 +2,6 @@ import { LinkProps } from 'next/link';
 import { ReactElement } from 'react';
 
 export interface ActiveLinkProps extends LinkProps {
-    isActive: boolean;
     children: ReactElement;
     shouldMatchExactHref: boolean;
 }
