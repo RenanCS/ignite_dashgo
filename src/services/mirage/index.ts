1e61ec3df36a8e86ed1010b57ec98fe0d63eee09
renan <renan@interprocess.com.br>
2022-01-28 16:53:54 -0300

    chore(mirage): adiciona dados fakes para inicializar a tela com informações

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,16 +1,31 @@
-import { createServer, Model, Server } from 'miragejs'
+import faker from 'faker';
+import { createServer, Factory, Model } from 'miragejs';
+import { User } from './interface';
 
-type User = {
-    name: string;
-    email: string;
-    created_at: string;
-}
 
 export const makeServer = () => {
     const server = createServer({
         models: {
             user: Model.extend<Partial<User>>({})
         },
+        factories: {
+            user: Factory.extend({
+                name(i: number) {
+                    return `User ${i + 1}`
+                },
+                email() {
+                    return faker.internet.email().toLowerCase();
+                },
+                createdAt() {
+                    return faker.date.recent(10);
+                }
+            })
+        },
+
+        seeds(server) {
+            server.createList('user', 20);
+        },
+
         routes() {
             this.namespace = 'api';
             this.timing = 750;
