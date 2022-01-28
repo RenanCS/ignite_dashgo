4aa37c1587f5bbdd4268c18af3f9cef5f67f6acc
renan <renan@interprocess.com.br>
2022-01-28 16:51:48 -0300

    feat(_app):adiciona querycliente na inicialização do app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,23 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from 'src/styles/theme'
 import { MenuDrawerProiver } from 'src/contexts/MenuDrawerContext'
 import { makeServer } from 'src/services/mirage'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      < MenuDrawerProiver>
-        <Component {...pageProps} />
-      </MenuDrawerProiver>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider resetCSS theme={theme}>
+        < MenuDrawerProiver>
+          <Component {...pageProps} />
+        </MenuDrawerProiver>
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
 
