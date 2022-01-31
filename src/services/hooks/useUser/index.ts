7e6a6dbc75e66a209cfebfcaf1c68bcdabb93a71
renan <renan@interprocess.com.br>
2022-01-31 15:25:19 -0300

    feat(useuser): adiciona hook de user para buscar usuários

---
+++ src/services/hooks/useUser/index.ts
@@ -0,0 +1,27 @@
+import { useQuery } from "react-query";
+import { getUsers } from "src/controllers/getUsers";
+import { User } from "src/pages/users/interface";
+import { FormatDate } from "src/util/formatDate";
+
+const QUERYKEY = 'users';
+
+const mappingAdapterUser = async () => {
+    const userResponse = await getUsers();
+
+    const usersFormated: User[] = userResponse.map(user => {
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            createdAt: FormatDate(user.createdAt)
+        }
+    });
+
+    return usersFormated;
+}
+
+export const useUsers = () => {
+    return useQuery<User[]>(QUERYKEY, async () => {
+        return await mappingAdapterUser();
+    });
+}
