ac50443cf669afaf0cb6b63d7f50d34bf0d15c43
renan <renan@interprocess.com.br>
2022-01-31 15:24:02 -0300

    chore(heading): adiciona isFeching no componente heading

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,6 +1,12 @@
-import { Heading as HeadingChakra } from '@chakra-ui/react';
-export const Heading: React.FC = () => {
+import { Heading as HeadingChakra, Spinner } from '@chakra-ui/react';
+import { HeadingProps } from './interface';
+export const Heading: React.FC<HeadingProps> = ({ isFetching }) => {
     return (
-        <HeadingChakra size="lg" fontWeight="normal">Usuários</HeadingChakra>
+        <HeadingChakra size="lg" fontWeight="normal">
+            Usuários
+            {
+                isFetching && <Spinner size="sm" color="gray.500" ml="4" />
+            }
+        </HeadingChakra>
     )
 }
---
+++ src/components/Heading/interface.ts
@@ -0,0 +1,3 @@
+export interface HeadingProps {
+    isFetching: boolean;
+}
