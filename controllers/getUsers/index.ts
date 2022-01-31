e63aacbdc2b8c3547058c3ce663a3402e72d3fbc
renan <renan@interprocess.com.br>
2022-01-31 14:23:32 -0300

    chore(Controllers): reorganização da estrutura da controller

--- controllers/getUsers/index.ts
+++
@@ -1,8 +0,0 @@
-import { api } from "src/services/axios"
-import { User } from "src/services/mirage/interface";
-
-export const getUsers = async (): Promise<User[]> => {
-    const result = await api.get<User[]>(`/users`);
-    debugger;
-    return result.data;
-}
