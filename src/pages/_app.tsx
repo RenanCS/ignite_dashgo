39332cd852a9ff841cb28f84ff51d1410b3ae266
renan <renan@interprocess.com.br>
2022-01-31 15:25:43 -0300

    feat(app): adiciona reactquerydevtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { theme } from 'src/styles/theme'
 import { MenuDrawerProiver } from 'src/contexts/MenuDrawerContext'
 import { makeServer } from 'src/services/mirage'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 if (process.env.NODE_ENV === 'development') {
   makeServer();
@@ -19,6 +20,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </MenuDrawerProiver>
       </ChakraProvider>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   )
 }
