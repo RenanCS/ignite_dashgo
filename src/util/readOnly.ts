87dd87d06205609fe079591a80eb1ef62aabbd70
renan <renan@interprocess.com.br>
2022-02-01 18:55:35 -0300

    feat(readonly): adiciona variáveis readonly global

---
+++ src/util/readOnly.ts
@@ -0,0 +1,6 @@
+export class Library {
+    public static readonly QUERYKEY: string = 'users';
+    public static readonly TIMEFRESHGRID: number = 1000 * 60 * 2;
+    public static readonly TIMEFRESHUSER: number = 1000 * 60 * 10;
+    public static readonly SIBLIGNSCOUNT: number = 1;
+}
