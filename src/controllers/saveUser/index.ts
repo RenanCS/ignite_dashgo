09c8e8ca37e93377d593a416d640ccca4648dbb7
renan <renan@interprocess.com.br>
2022-02-01 18:53:03 -0300

    chore(saveuser): corrige a forma de salvar os dados do usuário

---
+++ src/controllers/saveUser/index.ts
@@ -0,0 +1,17 @@
+import { CreateUserFormData } from "src/pages/users/interface";
+import { api } from "src/services/axios";
+import { UserRequest } from "./interface";
+
+export const saveUser = async (user: CreateUserFormData): Promise<CreateUserFormData> => {
+    const userRequest: UserRequest = {
+        email: user.email,
+        name: user.name,
+        password: user.password,
+        password_confirmation: user.password_confirmation,
+        createdAt: new Date().toLocaleDateString(),
+    }
+
+    const { data } = await api.post('users', { user: userRequest });
+
+    return data;
+}
