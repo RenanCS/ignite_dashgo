0bc88f59f4cf9ec7338d3b157d88877d2207de9f
renan <renan@interprocess.com.br>
2022-02-03 12:00:43 -0300

    feat(axios): add service axios authentication

---
+++ src/services/axios/autentication.ts
@@ -0,0 +1,68 @@
+import { fail } from 'assert';
+import axios, { AxiosError } from 'axios';
+import { parseCookies } from 'nookies';
+import { refreshTokenUser } from 'src/controllers/refreshTokenUser';
+import { Library } from 'src/util/readOnly';
+import { api } from '.';
+
+let cookies = parseCookies();
+let isRefreshing = false;
+let failedRequestsQueue = [];
+
+export const apiAuthentication = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_SEGURANCA,
+    headers: {
+        Authorization: `Bearer ${cookies[Library.DASHGOTOKEN]}`
+    }
+});
+
+apiAuthentication.interceptors.response.use(
+    response => {
+        return response
+    },
+    (error: AxiosError) => {
+        const is401 = error.response.status === 401;
+        const isTokenExpired = error.response.data?.code === Library.TOKENEXPIRED;
+
+        if (is401) {
+            if (isTokenExpired) {
+
+                cookies = parseCookies();
+
+                const { 'dashgo.refreshtoken': refreshToken } = cookies;
+
+                const originalConfig = error.config;
+
+                if (!isRefreshing) {
+                    isRefreshing = true;
+
+                    refreshTokenUser(refreshToken).then((newToken: string) => {
+                        failedRequestsQueue.forEach(request => request.onSuccess(newToken));
+                    })
+                        .catch(err => {
+                            failedRequestsQueue.forEach(request => request.onFailure(err));
+                        })
+                        .finally(() => {
+                            isRefreshing = false;
+                            failedRequestsQueue = [];
+                        });
+                }
+
+                return new Promise((resolve, reject) => {
+                    failedRequestsQueue.push({
+                        onSuccess: (newToken: string) => {
+                            originalConfig.headers['Authorization'] = `Bearer ${newToken}`;
+                            resolve(api(originalConfig))
+                        },
+                        onFailure: () => {
+                            reject(error);
+                        }
+                    })
+                })
+
+
+            } else {
+
+            }
+        }
+    });
