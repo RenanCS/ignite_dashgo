7b2be7e8b76cd9a82040d348b2e44bcf75490f87
renan <renan@interprocess.com.br>
2022-02-03 12:02:00 -0300

    chore(library): add vairables constants DASHGOTOKEN, DASHGOREFRESHTOKEN,  TIMETOLIVECOOKIE, TOKENEXPIRED

--- src/util/readOnly.ts
+++ src/util/readOnly.ts
@@ -3,4 +3,9 @@ export class Library {
     public static readonly TIMEFRESHGRID: number = 1000 * 60 * 2;
     public static readonly TIMEFRESHUSER: number = 1000 * 60 * 10;
     public static readonly SIBLIGNSCOUNT: number = 1;
+    public static readonly DASHGOTOKEN: string = 'dashgo.token';
+    public static readonly DASHGOREFRESHTOKEN: string = 'dashgo.refreshtoken';
+    public static readonly TIMETOLIVECOOKIE: number = 60 * 60 * 24 * 30;
+    public static readonly TOKENEXPIRED: string = 'token.expired';
+
 }
